d673b2e41fe5a7f6b3e4df78940d8d78fb161c2e
Anand Jagbir <anandjagbir@gmail.com>
2022-11-05 07:03:05 +0530

    Time: 37 ms (27.30%), Space: 17.9 MB (10.64%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -5,15 +5,21 @@ public:
         
         for(int i = 0; i < asteroids.size(); i++){
             bool collision = false;
+            /*collision condition- (asteroids[i] < 0 && st.top() > 0 */
             while(!st.empty() && asteroids[i] < 0 && st.top() > 0){
                 
+                /* case 1 - if prev asteroid is lesser than curr
+                so prev asteroid will explode(prev < curr) and i will keep checking previous
+                asteroid also*/
                 if(st.top() < -asteroids[i]){
                     st.pop();
                     continue;
-                }else if(st.top() == -asteroids[i]){
+                /*case 2 - prev and curr asteroid are equals both will explode*/
+                }else if(st.top() == -asteroids[i]){ 
                     st.pop();  
                     collision = true;
                     break;
+                /* if prev asteroid is larger than curr so current asteroid will explode*/
                 }else{
                     collision = true;
                     break;
