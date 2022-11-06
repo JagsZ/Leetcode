9b16ce78e70c2a55605f329cfb0fb8852fa5162d
Anand Jagbir <anandjagbir@gmail.com>
2022-11-06 21:06:58 +0530

    Time: 13 ms (22.99%), Space: 12.1 MB (8.62%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+       if(k==1){
+            string res=s, start =s;
+            string h = s.substr(1)+s[0];
+            s=h;
+            while(start!=s){
+                if(s.compare(res)<0){
+                    res=s;
+                }
+                h = s.substr(1)+s[0];
+                s=h;
+            }
+            return res;
+        }
+        sort(s.begin(), s.end());
+        return s;
+    }
+};
