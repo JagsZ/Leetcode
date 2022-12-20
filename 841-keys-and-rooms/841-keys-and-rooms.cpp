6e01883b01f39403588a3111b49236fcf05fa224
Anand Jagbir <anandjagbir@gmail.com>
2022-12-20 21:41:08 +0530

    Time: 20 ms (47.53%), Space: 10.7 MB (24.42%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -11,16 +11,9 @@ class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int m = rooms.size();
-        vector<vector<int>> adj_list(m, vector<int>());
         vector<bool> vis(m, false);
-        
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < rooms[i].size(); j++){
-                adj_list[i].push_back(rooms[i][j]);
-            }
-        }
-        
-        dfs(0, adj_list, vis);
+          
+        dfs(0, rooms, vis);
         for(int i = 0; i < m; i++){
             if(vis[i] == false)
                 return false;
