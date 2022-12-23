84bf998f2d311772061d6ac460fc653f5db3015f
Anand Jagbir <anandjagbir@gmail.com>
2022-12-23 14:45:21 +0530

    Time: 388 ms (67.64%), Space: 75.4 MB (77.33%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,40 +1,25 @@
 class Solution {
-    int solve(int index, int buy, int cap, vector<int> &prices, vector<vector<vector<int>>> &memo){
-        int profit = 0;
-        if(index == prices.size())
-            return 0;
-        
-        if(cap == 0)
-            return 0;
-        
-        if(memo[index][buy][cap] != -1)
-            return memo[index][buy][cap];
-        
-        if(buy){
-            profit = max((-prices[index] + solve(index+1, 0, cap, prices, memo)), (0 + solve(index+1, 1, cap, prices, memo)));
-        }else{
-            profit = max((prices[index] + solve(index+1, 1, cap-1, prices, memo)), (0 + solve(index+1, 0, cap, prices, memo)));
-        }
-        return memo[index][buy][cap] = profit;
-    }
+  
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+        vector<vector<int>> prev(2, vector<int>(3, 0));
+        vector<vector<int>> curr(2, vector<int>(3, 0));
         
         for(int index = n-1; index >= 0; index--){
             for(int buy = 0; buy <= 1; buy++){
                 for(int cap = 1; cap <= 2; cap++){
                     int profit = 0;
                     if(buy){
-                        profit = max(-prices[index] + dp[index+1][0][cap], dp[index+1][1][cap]);
+                        profit = max(-prices[index] + prev[0][cap], prev[1][cap]);
                     }else{
-                        profit = max(prices[index] + dp[index+1][1][cap-1], dp[index+1][0][cap]);
+                        profit = max(prices[index] + prev[1][cap-1], prev[0][cap]);
                     }
-                    dp[index][buy][cap] = profit;
+                    curr[buy][cap] = profit;
                 }
             }
+            prev = curr;
         }
-        return dp[0][1][2];
+        return prev[1][2];
     }
 };
