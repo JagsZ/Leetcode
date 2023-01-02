7a3059ac815043b8f04a3fe2874191e3621913d2
Anand Jagbir <anandjagbir@gmail.com>
2023-01-02 17:20:27 +0530

    Time: 65 ms (82.69%), Space: 48.4 MB (48.15%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,26 +1,15 @@
 class Solution {
-    bool solve(int index, int n, vector<int> & nums, vector<int> &memo){
-        if(index == nums.size()-1)
-            return true;
-        
-        if(index >= nums.size())
-            return false;
-        
-        if(memo[index] != -1)
-            return memo[index];
-        
-        for(int i = 1; i <= nums[index]; i++){
-            if(i+index < nums.size()){
-                if(solve(i+index, n, nums, memo) == true)
-                    return memo[i+index] = true;
-            }
-        }
-        return memo[index] = false;
-    }
+   
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> memo(n, -1);
-        return solve(0, n,  nums, memo);
+        int goal = n-1;
+        
+        for(int i = n-2; i >=0; i--){
+            if((i+nums[i]) >= goal){
+                goal = i;
+            }
+        }
+        return goal == 0? true : false;
     }
 };
