cd22457fb1bc1d7775cb36d118b76c2df85ed763
Anand Jagbir <anandjagbir@gmail.com>
2023-01-22 20:56:48 +0530

    Time: 588 ms (18.09%), Space: 37.1 MB (19.19%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -12,12 +12,12 @@ public:
     int minCut(string s) {
         int n = s.length();
         vector<int> dp(n+1 , 0);
-        //vector<vector<int>> palindrome_table(n, vector<int>(n, -1));
+        vector<vector<int>> palindrome_table(n, vector<int>(n, -1));
         
         for(int index = n-1; index >= 0; index--){
             int min_cost = INT_MAX;
             for(int i = index; i < n; i++){
-                if(isPalindrome(s, index, i)){
+                if(palindrome_table[index][i] != -1 ? palindrome_table[index][i]: palindrome_table[index][i] = isPalindrome(s, index, i)){
                     int cost = 1 + dp[i+1];
                     min_cost = min(min_cost, cost);
                 }
