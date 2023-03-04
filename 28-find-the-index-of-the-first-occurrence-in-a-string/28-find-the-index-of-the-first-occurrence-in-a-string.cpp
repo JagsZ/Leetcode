148622cd7423e52b6b60ca0bd2cddad5545b4ca5
Anand Jagbir <anandjagbir@gmail.com>
2023-03-04 17:14:08 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (69.90%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,29 +1,57 @@
-int mod = 1e9 + 7;
-int p = 31;
+
 class Solution {
 public:
-   int strStr(string haystack, string needle) {
-        int ans = -1, n = haystack.size(), m = needle.size();
-        
-        // If size of second string is greater than first string
-        if(n < m) {
-            return ans;
+   int hashValue(string string, int RADIX, int MOD, int m) {
+        int ans = 0;
+        long factor = 1;
+        for (int i = m - 1; i >= 0; i--) {
+            ans += ((int) (string[i] - 'a') * factor) % MOD;
+            factor = (factor * RADIX) % MOD;
         }
-        
-        for(int i=0; i<=n-m; i++) {
-            bool flag = true;
-            // Searching needle in haystack 
-            for(int j=i, k=0; k<m; j++,k++) {
-                if(haystack[j] != needle[k]) {
-                    flag = false;
-                    break;
+        return ans % MOD;
+    }
+
+    int strStr(string haystack, string needle) {
+        int m = needle.length();
+        int n = haystack.length();
+        if (n < m)
+            return -1;
+
+        // CONSTANTS
+        int RADIX = 26;
+        int MOD = 1000000033;
+        long MAX_WEIGHT = 1;
+
+        for (int i = 0; i < m; i++)
+            MAX_WEIGHT = (MAX_WEIGHT * RADIX) % MOD;
+
+        // Compute hash of needle
+        long hashNeedle = hashValue(needle, RADIX, MOD, m), hashHay = 0;
+
+        // Check for each m-substring of haystack, starting at index windowStart
+        for (int windowStart = 0; windowStart <= n - m; windowStart++) {
+            if (windowStart == 0) {
+                // Compute hash of the First Substring
+                hashHay = hashValue(haystack, RADIX, MOD, m);
+            } else {
+                // Update Hash using Previous Hash Value in O(1)
+                hashHay = ((hashHay * RADIX) % MOD - ((int) (haystack[windowStart - 1] - 'a') * MAX_WEIGHT) % MOD
+                        + (int) (haystack[windowStart + m - 1] - 'a') + MOD) % MOD;
+            }
+            // If the hash matches, Check Character by Character. 
+            // Because of Mod, spurious hits can be there.
+            if (hashNeedle == hashHay) {
+                for (int i = 0; i < m; i++) {
+                    if (needle[i] != haystack[i + windowStart]) {
+                        break;
+                    }
+                    if (i == m - 1) {
+                        return windowStart;
+                    }
                 }
             }
-            // If needle found
-            if(flag) {
-                return i;
-            }
         }
-        return ans;
+
+        return -1;
     }
 };
