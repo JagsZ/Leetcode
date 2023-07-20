28a90c19dd68c3a03282fcc02ed7cb3b2aa7dcb0
Anand Jagbir <anandjagbir@gmail.com>
2023-07-20 10:08:49 +0530

    Time: 18 ms (42.92%), Space: 17.7 MB (76.60%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+        
+        for(int i = 0; i < asteroids.size(); i++){
+            bool collision = false;
+            /*collision condition- (asteroids[i] < 0 && st.top() > 0 */
+            while(!st.empty() && asteroids[i] < 0 && st.top() > 0){
+                
+                /* case 1 - if prev asteroid is lesser than curr
+                so prev asteroid will explode(prev < curr) and i will keep checking previous
+                asteroid also*/
+                if(st.top() < -asteroids[i]){
+                    st.pop();
+                    continue;
+                /*case 2 - prev and curr asteroid are equals both will explode*/
+                }else if(st.top() == -asteroids[i]){ 
+                    st.pop();  
+                    collision = true;
+                    break;
+                /* case 3- if prev asteroid is larger than curr so current asteroid will explode*/
+                }else{
+                    collision = true;
+                    break;
+                }
+            }
+            if(collision == false)
+                st.push(asteroids[i]);
+        }
+        int size = st.size();
+        vector<int> ans(size, 0);
+        while(!st.empty()){
+            ans[size-1] = st.top();
+            st.pop();
+            size--;
+        }
+        return ans;
+    }
+};
