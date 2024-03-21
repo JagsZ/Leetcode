ec361546557d47582da311e3675c27542fb0b342
Anand Jagbir <anandjagbir@gmail.com>
2024-03-21 23:01:53 +0530

    Time: 106 ms (75.86%), Space: 238.4 MB (7.16%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -25,10 +25,24 @@ public:
         for(auto x : nums)
             sum += x;
         target = sum/2;
-        vector<vector<int>> memo(n, vector<int>(target+1, -1));
+        vector<vector<int>> dp(n, vector<int>(sum+1, 0));
         if(sum % 2 != 0)
             return false;
         
-        return solve(n-1, target, nums, memo);
+        for(int i = 0; i < n; i++){
+            dp[i][0] = true;
+        }
+        dp[0][nums[0]] = true;
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j <= target; j++){
+                int not_take = dp[i-1][j];
+                int take = false;
+                if(nums[i] <= j)
+                    take = dp[i-1][j-nums[i]];
+                dp[i][j] = (take | not_take);
+            }
+        }
+        return dp[n-1][target];
     }
 };
