36ef79c144cdf10fa6f2b1713c7e1d026278ded9
Anand Jagbir <anandjagbir@gmail.com>
2024-04-03 23:21:46 +0530

    Time: 7 ms (48.23%), Space: 15.9 MB (86.31%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,12 +1,45 @@
 class Solution {
+    int my_lower_bound(vector<int> &nums, int target){
+        int n = nums.size();
+        int l = 0, h = n-1, first = n;
+        while(l <= h){
+            int mid = (l+h)/2;
+            if(nums[mid] == target){
+                first = mid;
+                h = mid-1;
+            }else if(target > nums[mid]){
+                l = mid+1;
+            }else{
+                h = mid-1;
+            }
+        }
+        return first;
+    }
+    
+     int my_upper_bound(vector<int> &nums, int target){
+        int n = nums.size();
+        int l = 0, h = n-1, last = n;
+        while(l <= h){
+            int mid = (l+h)/2;
+            if(nums[mid] == target){
+               last = mid;
+               l = mid+1;
+            }else if(target > nums[mid]){
+                l = mid+1;
+            }else{
+                h = mid-1;
+            }
+        }
+        return last+1;
+    }
+    
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ans = {-1, -1};
         int n = nums.size();
-        auto lower = lower_bound(nums.begin(), nums.end(), target);
-        auto upper = upper_bound(nums.begin(), nums.end(), target);
-        int l = lower - nums.begin();
-        int u = upper - nums.begin();
+        int l  = my_lower_bound(nums, target);
+        int u = my_upper_bound(nums, target);
+  
         if(l == n || nums[l] != target) return ans;
         ans[0] = l;
         ans[1] = u-1;
